10e910ca42116381ff15a1f8bf837ca9c862e0bb
Matthias <matthias.druwe@odisee.be>
2022-12-01 12:00:08 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,21 @@
 fun main() {
+
+
     fun part1(input: List<String>): Int {
-        return input.size
+       return input.splitOnEmpty().maxOf { it.sum() }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.splitOnEmpty().sortedByDescending { it.sum() }.take(3).sumOf { it.sum() }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day01_example")
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
 }
+
--- src/Utils.kt
+++ src/Utils.kt
@@ -11,3 +11,16 @@ fun readInput(name: String) = File("src", "$name.txt").readLines()
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+
+fun List<String>.splitOnEmpty(): List<List<Int>>{
+    val output = mutableListOf(mutableListOf<Int>())
+    this.forEach {
+        if(it.toIntOrNull() != null){
+            output.last().add(it.toInt())
+        } else {
+            output.add(mutableListOf())
+        }
+    }
+    return output
+}
